29803de9f45830d898d310117fe777d800ff9bb7
timoles <timoles@users.noreply.github.com>
2019-04-03 10:27:29 +0200

    Debug info cleanup and made setScript function work

--- c/meterpreter/source/extensions/malleable/malleable.c
+++ c/meterpreter/source/extensions/malleable/malleable.c
@@ -14,11 +14,14 @@
 EnableDelayLoadMetSrv();
 
 DWORD test(Remote *remote, Packet *packet);
+DWORD setScript(Remote *remote, Packet *packet);
 
+static char * luaScript = "function encrypt(s);s = '<html> viewstate0=\"' ..s ..  '\" </html>';return s;end;";
 
 Command customCommands[] =
 {
 	COMMAND_REQ("malleable_test_command", test),
+	COMMAND_REQ("malleable_set_script", setScript),
 	COMMAND_TERMINATOR
 };
 
@@ -27,7 +30,7 @@ Command customCommands[] =
  */
 DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
 {
-	dprintf("TIMOTIMOTIMO malleableInit");
+	dprintf("MALLEABLE] Initializing malleable extension");
 	hMetSrv = remote->met_srv;
 
 	command_register_all(customCommands);
@@ -40,43 +43,61 @@ DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
  */
 DWORD __declspec(dllexport) DeinitServerExtension(Remote *remote)
 {
+	dprintf("MALLEABLE] De-initializing malleable extension");
 	command_deregister_all(customCommands);
 
 	return ERROR_SUCCESS;
 }
 
 void bail(lua_State *L, char *msg){
-	dprintf("[TIMOTIMOTIMOMALLEABLE] FATAL ERROR:  %s: %s",
+	dprintf("[MALLEABLE] FATAL ERROR:  %s: %s",
 		msg, lua_tostring(L, -1));
+}
+
+DWORD setScript(Remote *remote, Packet *packet)
+{
+	dprintf("[MALLEABLE] Executing set script command");
+	// Recieve Script
+	char * recievedScript = packet_get_tlv_value_string(packet, TLV_TYPE_MALLEABLE_INTERFACES);
+	dprintf("[MALLEABLE] Recieved and setting script: \"%s\"", recievedScript);
+	luaScript = _strdup(recievedScript);
+	// Send response
+	Packet *response = packet_create_response(packet);
+	int result = ERROR_SUCCESS;
+	packet_add_tlv_string(response, TLV_TYPE_MALLEABLE_INTERFACES, "New script set");
+	packet_transmit_response(result, remote, response);
+	dprintf("[MALLEABLE] Finished set script command");
+	return ERROR_SUCCESS;
 }
 
 DWORD test(Remote *remote, Packet *packet)
 {
-	dprintf("[TIMOTIMOTIMOMALLEABLE] testCommandStart");
+	dprintf("[MALLEABLE] testCommandStart");
+	dprintf("[TMPTMPTMPTMP] in script1: %s", luaScript);
 	lua_State *L;
 
 	L = luaL_newstate();                        /* Create Lua state variable */
 	luaL_openlibs(L);                           /* Load Lua libraries */
-
-	if (luaL_loadfile(L, "testFunction.lua")) /* Load but don't run the Lua script */
-		bail(L, "luaL_loadfile() failed");      /* Error out if file can't be read */
-
+	dprintf("[MALLEABLE] luaL_loadstring(): script: \"%s\"", luaScript);
+	if (luaL_loadstring(L, luaScript)) /* Load but don't run the Lua script */
+		bail(L, "luaL_loadstring() failed");      /* Error out if file can't be read */
+	dprintf("[MALLEABLE] lua_pcall(0,0,0) (init)");
 	if (lua_pcall(L, 0, 0, 0))                  /* PRIMING RUN. FORGET THIS AND YOU'RE TOAST */
 		bail(L, "lua_pcall() failed");          /* Error out if Lua file has an error */
 
 	lua_getglobal(L, "encrypt");
 	lua_pushstring(L, "evilMeterpreterData");
+	dprintf("[MALLEABLE] lua_pcall() (encrypt)");
 	if (lua_pcall(L, 1, 1, 0))
 		bail(L, "lua_pcall() failed");
 	const char *encryptedOut = lua_tostring(L, -1);
-	dprintf("Sending out: %s\n", encryptedOut);
+	dprintf("Got \"%s\" back from lua_tostring()", encryptedOut);
 
 	Packet *response = packet_create_response(packet);
 	int result = ERROR_SUCCESS;
-	dprintf("[TIMOTIMOTIMOMALLEABLE] adding string to response");
-	packet_add_tlv_string(response, TLV_TYPE_MALLEABLE_INTERFACES, "It works!"); 
+	packet_add_tlv_string(response, TLV_TYPE_MALLEABLE_INTERFACES, encryptedOut);
 	packet_transmit_response(result, remote, response);
-	
-	dprintf("[TIMOTIMOTIMOMALLEABLE] testCommandEnd");
+	lua_close(L);
+	dprintf("[MALLEABLE] testCommandEnd");
 	return ERROR_SUCCESS;
 }
